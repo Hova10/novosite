6b8fba2484816ed3a323ff336c000ddf597711ad
Hova10 <bruno@Brunos-MBP.lan>
2015-08-16 20:19:36 +0100

    final version 1.0

--- js/main.js
+++ js/main.js
@@ -31,13 +31,15 @@ app.controller("dataImagesWork", function ($scope) {
           { num: 1, category: 'WEBSITES', src: "1.png", description: 'My old Website. Received several Wotd nominations and 1 Wotd Award. ', url_details: "brunofeijao.html" },
           { num: 2, category: 'WEBSITES', src: "2.png", description: 'Portuguese website of Purina Gourmet. Totally redesigned from the ground up.', url_details: "purina.html" },
           { num: 3, category: 'WEBSITES', src: "3.png", description: 'website for a speech therapy clinic called "Sons para crescer".', url_details: "sonsparacrescer.html" },
-          { num: 4, category: 'PERSONAL PROJECTS', src: "4.png", description: 'My first project for my web developer course. This was the front-end project of the course.', url_details: "details.html" },
-          { num: 5, category: 'PERSONAL PROJECTS', src: "5.png", description: 'My second project for my web developer course. This was the back-end project of the course.', url_details: "details.html" },
-          { num: 6, category: 'FACEBOOK', src: "6.png", description: 'Coming soon', url_details: "details.html" },
-          { num: 7, category: 'FACEBOOK', src: "7.png", description: 'Coming soon', url_details: "details.html" },
-          { num: 8, category: 'FACEBOOK', src: "8.png", description: 'Coming soon', url_details: "details.html" },
-          { num: 9, category: 'FACEBOOK', src: "9.png", description: 'Coming soon', url_details: "details.html"}];
-
+          { num: 4, category: 'PERSONAL PROJECTS', src: "4.png", description: 'My first project for my web developer course. This was the front-end project of the course.', url_details: "opinatus.html" },
+          { num: 5, category: 'PERSONAL PROJECTS', src: "5.png", description: 'My second project for my web developer course. This was the back-end project of the course.', url_details: "eopinatus.html" },
+          { num: 6, category: 'FACEBOOK', src: "6.png", description: 'A facebook app for the vintage vans of Volkswagen', url_details: "vintagevans.html" },
+          { num: 7, category: 'FACEBOOK', src: "7.png", description: 'A facebook app for the "NOS ALIVE2015" summer fest', url_details: "nosalive.html" },
+          { num: 8, category: 'WEBSITES', src: "8.png", description: 'website for the summer contest by Friskies', url_details: "friskies.html" },
+          { num: 9, category: 'LANDING PAGES', src: "9.png", description: 'A landing page for the "Vintage Vans" fest by Volkswagen', url_details: "vwfest.html"},
+          { num: 10, category: 'WEBSITES', src: "10.png", description: 'website for the "Nestlé Moms"', url_details: "maesnestle.html" },
+          { num: 11, category: 'WEBSITES', src: "11.png", description: 'Coming soon', url_details: "../index.html" },
+    ];
 });
 
 
